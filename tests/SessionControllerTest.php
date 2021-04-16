9efb29efa2049b6a29db7f55697ef8ee9f5c6c8c
RafalMalik <rafalmalik.info@gmail.com>
2021-04-16 16:36:14 +0200

    Working with basic tests.

--- tests/SessionControllerTest.php
+++ tests/SessionControllerTest.php
@@ -3,15 +3,32 @@
 namespace App\Tests;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Response;
 
 class SessionControllerTest extends WebTestCase
 {
-    public function testSomething()
+    public function testCreate()
     {
         $client = static::createClient();
-        $crawler = $client->request('GET', '/');
+        $crawler = $client->request('POST', '/session/create', [], [], [], json_encode([
+            'gameId' => 1,
+            'amount' => 100,
+            'suid' => '870ff240-6c4a-4760-ae22-ac08feb40bd5'
+        ]));
 
-        $this->assertResponseIsSuccessful();
-        $this->assertSelectorTextContains('h1', 'Hello World');
+        $this->assertEquals(Response::HTTP_OK, $client->getResponse()->getStatusCode());
+        $response = json_decode($client->getResponse()->getContent(), true);
+    }
+
+    public function testRead()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('POST', '/session/create', [], [], [], json_encode([
+            'gameId' => 1,
+            'amount' => 100,
+            'suid' => '870ff240-6c4a-4760-ae22-ac08feb40bd5'
+        ]));
+
+        $this->assertEquals(Response::HTTP_OK, $client->getResponse()->getStatusCode());
     }
 }
